ef2685193284ddb17b0161eab7083f57d31dc1f2
MissDitch <mjtsloot@icloud.com>
2017-05-09 09:48:49 +0200

    cleaning up and renaming

--- js/main.js
+++ js/main.js
@@ -1,11 +1,9 @@
-function init() {
-    
+function init() {    
     var calculation = document.getElementById("calculation");
-    
     calculation.style.visibility = "hidden";
 
-    addListener("AC", clearAll);
-    addListener("CE", clearEntry);
+    addListener("AC", removeAll);
+    addListener("CE", removeEntry);
 
     addListener("zero", calculate);
     addListener("one", calculate);
@@ -27,54 +25,48 @@ function init() {
     addListener("equals", calculate);
 }
 
-var current, previous, leftOperand, rightOperand, calc, num = [], entries = [], clear = true;
+var current, previous, calc, number = [], entries = [], remove = true;
 var operators = {
-        '+': function(a, b) { return parseFloat(a) + parseFloat(b) },
-        '-': function(a, b) { return a - b },
-        '/': function(a, b) { return a / b },
-        'x': function(a, b) { return a * b }    
-    }; 
+    '+': function(a, b) { return parseFloat(a) + parseFloat(b) },
+    '-': function(a, b) { return a - b },
+    '/': function(a, b) { return a / b },
+    'x': function(a, b) { return a * b }    
+}; 
 
 function addListener(id, func) {
     var el = document.getElementById(id);
     return el.addEventListener("click", func); 
 }
 
-function clearAll() {
+function removeAll() {
     var output = document.getElementById("output");
     var calculation = document.getElementById("calculation");
     output.innerHTML = "";
     calculation.innerHTML = "";
     calculation.style.visibility = "hidden";
     entries.length = 0;
-    num.length = 0;
+    number.length = 0;
     calc = 0;
 }
 
-function clearEntry(e) {
+function removeEntry(e) {
     var output = document.getElementById("output");
     var calculation = document.getElementById("calculation");
-    if (num.length === 0) {
+    if (number.length === 0) {
         entries.pop();
         current = entries[entries.length - 1];
         output.innerHTML = "";
         if (entries.length === 0) {
             calculation.innerHTML = "";
             calculation.style.visibility = "hidden";
-        } else {
-            calculation.innerHTML = entries.join("");
-        }          
-    }
-    else {        
-        if (previous) {
-            current = previous;            
-        } 
-        num.pop(); 
-        output.innerHTML = num.join("");
-        if (entries.length === 0 && num.length === 0) {
-           clearAll();
-        } else {calculation.innerHTML = entries.join("").concat(num.join(""));
-        } 
+        } else { calculation.innerHTML = entries.join(""); }          
+    } else {        
+        if (previous) { current = previous; } 
+        number.pop(); 
+        output.innerHTML = number.join("");
+        if (entries.length === 0 && number.length === 0) {
+           removeAll();
+        } else { calculation.innerHTML = entries.join("").concat(number.join("")); } 
     }
 }  
 
@@ -86,61 +78,54 @@ function calculate(e) {
     var regExOperators = /[-+x/]/;   
     var entry = e.target.textContent;    
     previous = current;
-    current = entry;  
-
+    current = entry; 
     calculation.style.visibility = "visible"; 
 
-    if (clear) {output.innerHTML = "";}
+    if (remove) {output.innerHTML = "";}
     
     if (regExNumbers.test(entry)) {    
-        if (regExEquals.test(calculation.textContent)) { clearAll();}
-        clear = false;                
-        num.push(entry);
+        if (regExEquals.test(calculation.textContent)) { removeAll();} 
+        remove = false;                
+        number.push(entry);
         output.innerHTML += entry;
         calculation.innerHTML += entry;       
-    } 
-    else {  
+    } else {  
         if (regExOperators.test(previous) || regExEquals.test(previous)){
             output.innerHTML = previous; 
             return; 
         }
-        clear = true;
+        remove = true;
         output.innerHTML = entry;
-        var numString = num.join("");
-        if (regExNumbers.test(numString)) {
-            entries.push(parseFloat(numString)) ;
-            calc = numString;
+        var numberString = number.join("");
+        if (regExNumbers.test(numberString)) {
+            entries.push(parseFloat(numberString)) ;
+            calc = numberString;
         }        
         entries.push(entry);        
-        num = [];         
+        number = [];         
 
         if (entries.length < 3) {  //there is no right operand yet       
             calculation.innerHTML += entry;
-            console.log(calculation.textContent);
             if (regExEquals.test(calculation.textContent))  { 
-                calculation.innerHTML = entries.join(""); // get rid of last calculation
+                calculation.innerHTML = entries.join(""); 
             }             
-        }
-        else  { //there is a right operand now
-            leftOperand = entries[0];
-            rightOperand = entries[2];   
+        } else { 
             var prop = entries[1];          
-            calc = operators[prop](leftOperand, rightOperand);  
-            if (!Number.isInteger(calc))  { calc = calc.toFixed(3); } 
+            calc = operators[prop](entries[0], entries[2]);  
+            if (!Number.isInteger(calc))  { calc = calc.toFixed(2); } 
            // console.log(entries);           
-            if (regExEquals.test(entry)) { // "="
+            if (regExEquals.test(entry)) { 
                 output.innerHTML = calc;
                 calculation.innerHTML += "=" + calc;
                 entries.splice(0, 4, calc);  
                 current = calc;               
-            } 
-            else { calculation.innerHTML = calc + entry; }
+            } else { calculation.innerHTML = calc + entry; }
             entries.splice(0, 3, calc);             
         }       
     }
 }
 
-function countDigits(num) {
+function countDigits(number) {
     
 }
 
@@ -148,18 +133,18 @@ init();
 
 /* 
 digits are shown in output
-store first number when operator is entered 
-replace number by operator in output
+store first numberber when operator is entered 
+replace numberber by operator in output
 what kind of operator is chosen?
 store operator
-replace operator  by second number in output
+replace operator  by second numberber in output
 
-when operator key is pressed again, store second number, make calculation first and store result
+when operator key is pressed again, store second numberber, make calculation first and store result
 before making next calculation
 
-when equals key is pressed store second number, make calculation and show result
+when equals key is pressed store second numberber, make calculation and show result
 
-when CE key is pressed, the last entry is removed, either number or operator
+when CE key is pressed, the last entry is removed, either numberber or operator
 
 
 */
