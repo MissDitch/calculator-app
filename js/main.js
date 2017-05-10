40ec6af3e3fc92eb8eaae72a910631f41549d42e
MissDitch <mjtsloot@icloud.com>
2017-05-10 10:02:33 +0200

    adding comments

--- js/main.js
+++ js/main.js
@@ -53,7 +53,8 @@ function removeAll() {
 function removeEntry(e) {
     var output = document.getElementById("output");
     var calculation = document.getElementById("calculation");
-    if (number.length === 0) {
+
+    if (number.length === 0) { // no number in the making
         entries.pop();
         current = entries[entries.length - 1];
         output.innerHTML = "";
@@ -65,6 +66,7 @@ function removeEntry(e) {
         if (previous) { current = previous; } 
         number.pop(); 
         output.innerHTML = number.join("");
+
         if (entries.length === 0 && number.length === 0) {
            removeAll();
         } else { calculation.innerHTML = entries.join(" ").concat(" " + number.join("")); } 
@@ -73,6 +75,7 @@ function removeEntry(e) {
 
 function toggleNegative() {
     var regExEquals = /=/; 
+
     if (regExEquals.test(calculation.textContent)) { removeAll();}
     
     if (!isNegative) {
@@ -94,30 +97,37 @@ function calculate(e) {
     var regExOperators = /^[-+x/]$/;    
     var entry = e.target.textContent;    
     previous = current;
-    current = entry; 
-    calculation.style.visibility = "visible"; 
+    current = entry;         
 
     if (remove) {output.innerHTML = "";}
     
-    if (regExNumbers.test(entry)) {    
-        if (entries.length === 1) { removeAll();} 
+    if (regExNumbers.test(entry)) { // digit is entered   
+        calculation.style.visibility = "visible"; 
+
+        if (entries.length === 1) { // array contains result of last calculation
+            removeAll();} // new calculation can start
         remove = false;  
         operate = true;              
         number.push(entry);
         output.innerHTML = number.join("");
         calculation.innerHTML = entries.join(" ").concat(" " + number.join(""));       
-    } else {  
-        if (regExOperators.test(previous) || regExEquals.test(previous)) {
-            output.innerHTML = previous; 
+    } else { // operator or equals sign is entered
+        if (!operate) { return; } 
+
+        if (regExOperators.test(previous) || regExEquals.test(previous)) { 
             operate = false;
-            return;  // no input of operator or "=" when last entry was an operator or there is nothing to calculate yet
-        }     
-        if ( regExEquals.test(entry) && entries.length === 0)   {
-            return;
+            return;  // entering 2 operators is impossible
+        }  
+
+        if (entries.length === 0 && number.length === 0) {
+            operate = false;
+            calculation.style.visibility = "hidden";
+            return;  //starting calculation with operator or equals sign is impossible
         }
         remove = true;
         output.innerHTML = entry;
         var numberString = number.join("");
+        //use BigNumber here ? or in operator object?
         if (regExNumbers.test(numberString)) {
             entries.push(parseFloat(numberString));
             calc = numberString;
@@ -126,58 +136,25 @@ function calculate(e) {
         number = []; 
         isNegative = false;        
 
-        if (entries.length < 3) {  //there is no right operand yet       
-            calculation.innerHTML += " " + entry;
-            if (regExEquals.test(calculation.textContent))  { 
-                calculation.innerHTML = entries.join(" "); 
-            }             
-        } else { 
+        if (entries.length < 3) {  // only left operand and operator in array      
+            calculation.innerHTML += " " + entry; 
+            if (regExEquals.test(calculation.textContent))  { // previous calculation
+                calculation.innerHTML = entries.join(" "); // show new calculation
+            }                     
+        } else { // right operand and equals sign (or new operator): make calculation
             var prop = entries[1];          
             calc = operators[prop](entries[0], entries[2]);  
-            if (!Number.isInteger(calc))  { calc = calc.toFixed(2); } 
 
             if (regExEquals.test(entry)) { 
                 output.innerHTML = calc;
                 calculation.innerHTML += " = " + calc;
                 entries.splice(0, 4, calc);  
                 current = calc;               
-            } else { calculation.innerHTML = calc + " " + entry; }
-            entries.splice(0, 3, calc);             
+           } else {   
+                calculation.innerHTML = calc + " " + entry; }
+                entries.splice(0, 3, calc);     // calculation becomes left operand for next calculation        
         }       
     }
 }
 
-function countDigits(number) {
-    
-}
-
 init();
-
-/* 
-digits are shown in output
-store first numberber when operator is entered 
-replace numberber by operator in output
-what kind of operator is chosen?
-store operator
-replace operator  by second numberber in output
-
-when operator key is pressed again, store second numberber, make calculation first and store result
-before making next calculation
-
-when equals key is pressed store second numberber, make calculation and show result
-
-when CE key is pressed, the last entry is removed, either numberber or operator
-
-
-*/
-
-/*
-var operators = {
-    '+': function(a, b) { return a + b },
-    '<': function(a, b) { return a < b },
-     // ...
-};
-
-var op = '+';
-alert(operators[op](10, 20));
-   */
